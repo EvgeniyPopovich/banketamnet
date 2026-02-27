e6cd0b3af37552316e887b2b629117a96e63c8fc
evgeniy popovich <zhp.97@mail.ru>
2026-02-27 11:33:25 +0500

    Подключение бд

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php
+class Db {
+    private static $pdo = null;
+    public static function getConnection() {
+        if (self::$pdo === null) {
+            $host = "MySQL-8.4";
+            $dbname = "banketam";
+            $user = "root";
+            $pass = "";
+            $dsn = "mysql::host=$host;dbname=$dbname;charset=utf8";
+            self::$pdo = new PDO($dsn, $user, $pass);
+        }
+        return self::$pdo;
+    }
+}
